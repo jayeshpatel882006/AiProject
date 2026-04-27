31d5f156c93872ebb6196c075bbb1144fa4538bb
unknown <jayeshpatelswami00@gmail.com>
2026-04-27 12:28:01 +0530

    updated backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_API_URL || "http://localhost:5000/api",
+  // baseURL: process.env.BACKEND_API_URL || "http://localhost:5000/api",
+  baseURL:
+    process.env.BACKEND_API_URL ||
+    "https://aiproject-production-4268.up.railway.app/api",
   timeout: 30000,
   headers: { "Content-Type": "application/json" },
 });
